981a8d9aa7e81c535dfb5779215d84761c814eac
Pablo Derendinger <pderendinger@gmail.com>
2019-10-24 21:26:36 -0700

    Removing ThreeJS example

--- src/threejs/start.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-const app = express();
-app.use(express.static('public'));
-app.use('/model', express.static(__dirname + '/model'));
-app.use('/three', express.static(__dirname + '/three'));
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
-);
